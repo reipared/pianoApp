ac269e8309f67ec3e98e0f48718c739b77a060ba
Reinaldo <reinaldopared@gmail.com>
2023-12-20 13:00:43 -0400

    Update

---
+++ main.js
@@ -0,0 +1,17 @@
+const keys = document.querySelectorAll(".key");
+const regulars = document.querySelectorAll(".key.white");
+const sharps = document.querySelectorAll(".key.black");
+
+keys.forEach((key) => {
+	key.addEventListener("click", () => playNote(key));
+});
+
+let playNote = (key) => {
+	const noteSound = document.getElementById(key.dataset.note);
+	noteSound.currentTime = 0;
+	noteSound.play();
+	key.classList.add("active");
+	noteSound.addEventListener("ended", () => {
+		key.classList.remove("active");
+	});
+};
